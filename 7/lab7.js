f6f864d236e3a4bcc4891f4e57331c9b0feb7b76
MCast1e <creedarmy1@gmail.com>
2020-11-09 22:16:06 +0300

    Delete lab7.js

--- 7/lab7.js
+++
@@ -1,4 +0,0 @@
-let promise = fetch('https://jsonplaceholder.typicode.com/posts?userId=1')
-.then((response) => {return response.json();})
-.then((data) => {console.log(data);})
-.catch((error) => {console.error('Ошибочка:', error)});
